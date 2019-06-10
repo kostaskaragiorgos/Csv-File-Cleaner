48fabbf59349dc35ff1023478bd23e24e85f57ce
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2019-06-10 19:16:39 +0300

    Add files via upload

---
+++ csvfilecleaner.py
@@ -0,0 +1,55 @@
+from tkinter import *
+from tkinter import messagebox as msg
+from tkinter import filedialog
+
+class CSV_FILECLEANER():
+    def __init__(self,master):
+        self.master = master
+        self.master.title("CSV FILE CLEANER")
+        self.master.geometry("250x120")
+        self.master.resizable(False,False)
+        self.insertf = Button(self.master,text= "INSERT A CSV FILE",command = self.insertf)
+        self.insertf.pack()
+        
+        self.deldup = Button(self.master,text = "Delete Duplicates")
+        self.deldup.pack()
+        
+        
+        self.menu = Menu(self.master)
+        
+        self.file_menu = Menu(self.menu,tearoff = 0)
+        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
+        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        
+        
+        self.about_menu = Menu(self.menu,tearoff = 0)
+        self.about_menu.add_command(label = "About",accelerator = 'Ctrl+I',command=self.aboutmenu)
+        self.menu.add_cascade(label="About",menu=self.about_menu)
+        
+        self.help_menu = Menu(self.menu,tearoff = 0)
+        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
+        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        
+        self.master.config(menu=self.menu)
+        
+
+    def insertf(self):
+        pass
+    
+    def aboutmenu(self):
+        pass
+    
+    def helpmenu(self):
+        pass
+    
+    def exitmenu(self):
+        if msg.askokcancel("Quit?", "Really quit?"):
+            self.master.destroy()
+    
+def main():
+    root=Tk()
+    CC = CSV_FILECLEANER(root)
+    root.mainloop()
+    
+if __name__=='__main__':
+    main()

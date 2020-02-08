c4a9024757f429f1ae6635c1634174e81fb1de13
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-08 11:22:54 +0200

    Update csv_file_cleaner.py

--- csv_file_cleaner.py
+++ csv_file_cleaner.py
@@ -62,7 +62,12 @@ class CSV_FILECLEANER():
         self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
         self.master.bind('<Control-F1>',lambda event: self.helpmenu())
         self.master.bind('<Control-i>',lambda event:self.aboutmenu())
-        
+
+    
+    def closef(self):
+        self.pandascheck = ""
+        self.filename = ""
+        msg.showinfo("SUCCESS","THE CSV FILE CLOSED SUCCESSFULLY")
         
     def savef(self):
         """ saves the csv file """ 
@@ -126,15 +131,17 @@ class CSV_FILECLEANER():
                 
     def insertf(self):
         """ inserts the csv file """
-
-        self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
-                                                   filetypes=(("csv files","*.csv"),("all files","*.*")))
-        if self.filename.endswith('.csv'):
-            self.pandascheck = pd.read_csv(self.filename)
-            msg.showinfo("SUSSESSFULL INSERT","YOUR CSV FILE HAS SUCCESFULLY INSERTED")
+        if (".csv" in self.filename) == TRUE:
+            msg.showerror("ERROR", "A CSV FILE IS ALREADY OPEN")
         else:
-            msg.showerror("INSERT A CSV","YOU HAVE TO INSERT A CSV FILE")
-        
+            self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
+                                                   filetypes=(("csv files","*.csv"),("all files","*.*")))
+            if self.filename.endswith('.csv'):
+                self.pandascheck = pd.read_csv(self.filename)
+                msg.showinfo("SUSSESSFULL INSERT","YOUR CSV FILE HAS SUCCESFULLY INSERTED")
+            else:
+                msg.showerror("INSERT A CSV","YOU HAVE TO INSERT A CSV FILE")
+
     def aboutmenu(self):
         msg.showinfo("About","CSV FILE CLEANER \nVersion 1.0")
         

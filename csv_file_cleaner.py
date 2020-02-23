173097b62eb49bcbc5260d06c732f275c560ea01
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-23 15:57:50 +0200

    Update csv_file_cleaner.py

--- csv_file_cleaner.py
+++ csv_file_cleaner.py
@@ -1,166 +1,142 @@
-
-from tkinter import *
+from tkinter import Tk, Button, Menu
 from tkinter import messagebox as msg
 from tkinter import filedialog
-
 import pandas as pd
-
 class CSV_FILECLEANER():
-    def __init__(self,master):
+    def __init__(self, master):
         self.master = master
         self.master.title("CSV FILE CLEANER")
         self.master.geometry("250x120")
-        self.master.resizable(False,False)
+        self.master.resizable(False, False)
         self.pandascheck = ""
         self.filename = ""
-        self.insertfb = Button(self.master,text= "INSERT A CSV FILE",command = self.insertf)
+        self.insertfb = Button(self.master, text="INSERT A CSV FILE", command=self.insertf)
         self.insertfb.pack()
-
-        self.deldupb = Button(self.master,text = "Delete Duplicates",command = self.dropdp)
+        self.deldupb = Button(self.master, text="Delete Duplicates", command=self.dropdp)
         self.deldupb.pack()
-        
-        self.dropnab = Button(self.master,text = "Drop Non Values",command = self.dropnv)
+        self.dropnab = Button(self.master, text="Drop Non Values", command=self.dropnv)
         self.dropnab.pack()
-        
-        self.fixnab = Button(self.master,text = "Fix Non Values ",command = self.fixnv)
+        self.fixnab = Button(self.master, text="Fix Non Values ", command=self.fixnv)
         self.fixnab.pack()
-        
-        #menu 
-
+        #menu
         self.menu = Menu(self.master)
-        
-        self.file_menu = Menu(self.menu,tearoff = 0)
-        self.file_menu.add_command(label = "Insert csv file",accelerator = 'Ctrl+O',command = self.insertf)
-        self.file_menu.add_command(label = "Save csv file",accelerator = 'Ctrl+S',command = self.savef)
-        self.file_menu.add_command(label = "Close csv", accelerator = 'Ctrl + F5', state="disable", command = self.closef)
-        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
-        self.menu.add_cascade(label = "File",menu=self.file_menu)
-        
-        self.edit_menu = Menu(self.menu,tearoff = 0)
-        self.edit_menu.add_command(label  = 'Delete duplicates',accelerator = 'Ctrl + D',command = self.dropdp)
-        self.edit_menu.add_command(label = 'Drop Non Values',accelerator = 'Ctrl + N',command = self.dropnv)
-        self.edit_menu.add_command(label = 'Fix Non Values',accelerator = 'Ctrl + F',command = self.fixnv)
-        self.menu.add_cascade(label = 'Edit',menu =self.edit_menu)
-                
-        
-        self.about_menu = Menu(self.menu,tearoff = 0)
-        self.about_menu.add_command(label = "About",accelerator = 'Ctrl+I',command=self.aboutmenu)
+        self.file_menu = Menu(self.menu, tearoff=0)
+        self.file_menu.add_command(label="Insert csv file", accelerator='Ctrl+O', command=self.insertf)
+        self.file_menu.add_command(label="Save csv file", accelerator='Ctrl+S', command=self.savef)
+        self.file_menu.add_command(label="Close csv", accelerator='Ctrl + F5', state="disable", command=self.closef)
+        self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.edit_menu = Menu(self.menu, tearoff=0)
+        self.edit_menu.add_command(label='Delete duplicates', accelerator='Ctrl + D', command=self.dropdp)
+        self.edit_menu.add_command(label='Drop Non Values', accelerator='Ctrl + N', command=self.dropnv)
+        self.edit_menu.add_command(label='Fix Non Values', accelerator='Ctrl + F', command=self.fixnv)
+        self.menu.add_cascade(label='Edit', menu=self.edit_menu)
+        self.about_menu = Menu(self.menu, tearoff=0)
+        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
         self.menu.add_cascade(label="About",menu=self.about_menu)
-        
-        self.help_menu = Menu(self.menu,tearoff = 0)
-        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
-        self.menu.add_cascade(label="Help",menu=self.help_menu)
-        
+        self.help_menu = Menu(self.menu, tearoff=0)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.menu.add_cascade(label="Help", menu=self.help_menu)
         self.master.config(menu=self.menu)
-        
-        self.master.bind('<Control-o>',lambda event: self.insertf())
-        self.master.bind('<Control-s>',lambda event: self.savef())
-        self.master.bind('<Control-F5>',lambda event: self.closef())
-        self.master.bind('<Control-d>',lambda event: self.dropdp())
-        self.master.bind('<Control-n>',lambda event: self.dropnv())
-        self.master.bind('<Control-f>',lambda event: self.fixnv())
-        
-        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
-        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
-        self.master.bind('<Control-i>',lambda event:self.aboutmenu())
-
-    
+        self.master.bind('<Control-o>', lambda event: self.insertf())
+        self.master.bind('<Control-s>', lambda event: self.savef())
+        self.master.bind('<Control-F5>', lambda event: self.closef())
+        self.master.bind('<Control-d>', lambda event: self.dropdp())
+        self.master.bind('<Control-n>', lambda event: self.dropnv())
+        self.master.bind('<Control-f>', lambda event: self.fixnv())
+        self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
+        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
+        self.master.bind('<Control-i>', lambda event:self.aboutmenu())
     def closef(self):
         """ closes the csv file """ 
         self.pandascheck = ""
         self.filename = ""
-        msg.showinfo("SUCCESS","THE CSV FILE CLOSED SUCCESSFULLY")
-        self.file_menu.entryconfig("Close csv",state = "disable")
-
+        msg.showinfo("SUCCESS", "THE CSV FILE CLOSED SUCCESSFULLY")
+        self.file_menu.entryconfig("Close csv", state="disable")
     def savef(self):
         """ saves the csv file """ 
         if self.filename.endswith('.csv') == False:
-            msg.showerror("NO INPORT","NO CSV FILE IMPORTED")
+            msg.showerror("NO INPORT", "NO CSV FILE IMPORTED")
         else:
-            filenamesave = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+            filenamesave = filedialog.asksaveasfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
             if filenamesave.endswith('.csv'):
-                msg.showinfo("SUCCESS","THE CSV FILE SAVED SUCCESSFULLY")
-                self.pandascheck.to_csv(filenamesave,index = False)
-                
+                msg.showinfo("SUCCESS", "THE CSV FILE SAVED SUCCESSFULLY")
+                self.pandascheck.to_csv(filenamesave, index=False)
     def dropdp(self):
         """ Deletes duplicates"""
         if (".csv" in self.filename) == False:
-            msg.showerror("NO INPORT","NO CSV FILE IMPORTED")
+            msg.showerror("NO INPORT", "NO CSV FILE IMPORTED")
         else:
             self.pandascheck.drop_duplicates(keep=False)
-            msg.showinfo("DROP DUPLICATES", "DUPLICATES HAS SUCESSFULLY DROPED" )
-            save = msg.askyesno("SAVE FILE","DO YOU WANT TO SAVE A NEW CSV FILE??")
-            if save == True:
-                filenamesave = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+            msg.showinfo("DROP DUPLICATES", "DUPLICATES HAS SUCESSFULLY DROPED")
+            save = msg.askyesno("SAVE FILE", "DO YOU WANT TO SAVE A NEW CSV FILE??")
+            if save:
+                filenamesave = filedialog.asksaveasfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
                 if filenamesave.endswith('.csv'):
-                    msg.showinfo("SUCCESS","THE CSV FILE SAVED SUCCESSFULLY")
-                    self.pandascheck.to_csv(filenamesave,index = False)
+                    msg.showinfo("SUCCESS", "THE CSV FILE SAVED SUCCESSFULLY")
+                    self.pandascheck.to_csv(filenamesave, index=False)
             else:
-                msg.showerror("NO SAVE","NO FILE SAVED")
-
+                msg.showerror("NO SAVE", "NO FILE SAVED")
     def dropnv(self):
         """ drops missing values """ 
         if (".csv" in self.filename) == False:
-            msg.showerror("NO INPORT","NO CSV FILE IMPORTED")
+            msg.showerror("NO INPORT", "NO CSV FILE IMPORTED")
         else:
             self.pandascheck.dropna
-            msg.showinfo("MISSING VALUES","MISSING VALUES HAS SUCCESSFULLY REMOVED")
-            save = msg.askyesno("SAVE FILE","DO YOU WANT TO SAVE A NEW CSV FILE??")
-            if save == True:
-                filenamesave = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+            msg.showinfo("MISSING VALUES", "MISSING VALUES HAS SUCCESSFULLY REMOVED")
+            save = msg.askyesno("SAVE FILE", "DO YOU WANT TO SAVE A NEW CSV FILE??")
+            if save:
+                filenamesave = filedialog.asksaveasfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
                 if filenamesave.endswith('.csv'):
-                    msg.showinfo("SUCCESS","THE CSV FILE SAVED SUCCESSFULLY")
-                    self.pandascheck.to_csv(filenamesave,index = False)
+                    msg.showinfo("SUCCESS", "THE CSV FILE SAVED SUCCESSFULLY")
+                    self.pandascheck.to_csv(filenamesave, index=False)
             else:
-                msg.showerror("NO SAVE","NO FILE SAVED")
-    
+                msg.showerror("NO SAVE", "NO FILE SAVED")
     def fixnv(self):
         if (".csv" in self.filename) == False:
-            msg.showerror("NO INPORT","NO CSV FILE IMPORTED")
+            msg.showerror("NO INPORT", "NO CSV FILE IMPORTED")
         else:
             na = self.pandascheck.isna()
             if 'F' in str(na):
-                msg.showinfo("NO MISSING VALUES","THERE ARE NO MISSING VALUES")
+                msg.showinfo("NO MISSING VALUES", "THERE ARE NO MISSING VALUES")
             else:
                 self.pandascheck.fillna(0)
                 msg.showinfo("MISSING VALUES", "MISSING VALUES FILLED WITH 0(ZERO) ")
-                save = msg.askyesno("SAVE FILE","DO YOU WANT TO SAVE A NEW CSV FILE??")
-                if save == True:
-                    filenamesave = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+                save = msg.askyesno("SAVE FILE", "DO YOU WANT TO SAVE A NEW CSV FILE??")
+                if save:
+                    filenamesave = filedialog.asksaveasfilename(initialdir="/", title="Select file", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
                     if filenamesave.endswith('.csv'):
-                        msg.showinfo("SUCCESS","THE CSV FILE SAVED SUCCESSFULLY")
-                        self.pandascheck.to_csv(filenamesave,index = False)
+                        msg.showinfo("SUCCESS", "THE CSV FILE SAVED SUCCESSFULLY")
+                        self.pandascheck.to_csv(filenamesave, index=False)
                 else:
-                    msg.showerror("NO SAVE","NO FILE SAVED")
-                
+                    msg.showerror("NO SAVE", "NO FILE SAVED")
     def insertf(self):
         """ inserts the csv file """
-        if (".csv" in self.filename) == TRUE:
+        if (".csv" in self.filename):
             msg.showerror("ERROR", "A CSV FILE IS ALREADY OPEN")
         else:
-            self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
-                                                   filetypes=(("csv files","*.csv"),("all files","*.*")))
+            self.filename = filedialog.askopenfilename(initialdir="/", title="Select csv file",
+                                                   filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
             if self.filename.endswith('.csv'):
                 self.pandascheck = pd.read_csv(self.filename)
-                self.file_menu.entryconfig("Close csv",state = "active")
-                msg.showinfo("SUSSESSFULL INSERT","YOUR CSV FILE HAS SUCCESFULLY INSERTED")
+                self.file_menu.entryconfig("Close csv", state="active")
+                msg.showinfo("SUSSESSFULL INSERT", "YOUR CSV FILE HAS SUCCESFULLY INSERTED")
             else:
-                msg.showerror("INSERT A CSV","YOU HAVE TO INSERT A CSV FILE")
-
+                msg.showerror("INSERT A CSV", "YOU HAVE TO INSERT A CSV FILE")
     def aboutmenu(self):
-        msg.showinfo("About","CSV FILE CLEANER \nVersion 1.0")
-        
+        """ about menu function """
+        msg.showinfo("About", "CSV FILE CLEANER \nVersion 1.0")
     def helpmenu(self):
-        msg.showinfo("Help" ,"THE PURPUSE OF THIS APP IS TO HELP YOU CLEAN YOUR 'MESSY' CSV FILES ")
-    
+        """ help menu function """
+        msg.showinfo("Help", "THE PURPUSE OF THIS APP IS TO HELP YOU CLEAN YOUR 'MESSY' CSV FILES ")
     def exitmenu(self):
+        """ exit menu function"""
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
-    
 def main():
+    """ main function """
     root=Tk()
-    CC = CSV_FILECLEANER(root)
+    CSV_FILECLEANER(root)
     root.mainloop()
-    
 if __name__=='__main__':
     main()

d41d56bfa57f9fea514f99e6314fc0b2f0584d30
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-04-03 14:12:39 +0300

    Update csv_file_cleaner_v2.py

--- csv_file_cleaner_v2.py
+++ csv_file_cleaner_v2.py
@@ -2,7 +2,8 @@
 csv_file_cleaner_version_2
 """
 from tkinter import Tk, Menu
-from tkinter import messagebox as msg
+from tkinter import messagebox as msg, filedialog
+import pandas as pd
 def helpmenu():
     """ help menu funciton """
 def aboutmenu():
@@ -14,10 +15,18 @@ class CsvFileCleaner():
         self.master.title("Csv-File-Cleaner")
         self.master.geometry("250x120")
         self.master.resizable(False, False)
+        self.filename = ""
         self.menu = Menu(self.master)
         self.file_menu = Menu(self.menu, tearoff=0)
+        self.file_menu.add_command(label="Insert a csv file", accelerator='Ctrl+O', command=self.insertfile)
+        self.file_menu.add_command(label="Close file", command=self.closefile)
         self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
         self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.dup_menu = Menu(self.menu, tearoff=0)
+        self.dup_menu.add_command(label="Delete all duplicates")
+        self.dup_menu.add_command(label="Delete all duplicates except first")
+        self.dup_menu.add_command(label="Delete all duplicates except last")
+        self.menu.add_cascade(label="Duplicates", menu=self.dup_menu)
         self.about_menu = Menu(self.menu, tearoff=0)
         self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=aboutmenu)
         self.menu.add_cascade(label="About", menu=self.about_menu)
@@ -28,6 +37,24 @@ class CsvFileCleaner():
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
         self.master.bind('<Control-F1>', lambda event: helpmenu())
         self.master.bind('<Control-i>', lambda event: aboutmenu())
+    def closefile(self):
+        if not ".csv" in self.filename:
+            msg.showerror("ERROR", "NO CSV TO CLOSE")
+        else:
+            self.filename = ""
+            msg.showinfo("SUSSESS", "YOUR CSV FILE HAS SUCCESFULLY CLOSED")
+    def insertfile(self):
+        """ inserts the csv file """
+        if ".csv" in self.filename:
+            msg.showerror("ERROR", "A CSV FILE IS ALREADY OPEN")
+        else:
+            self.filename = filedialog.askopenfilename(initialdir="/", title="Select csv file",
+                                                       filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+            if self.filename.endswith('.csv'):
+                self.pandascheck = pd.read_csv(self.filename)
+                msg.showinfo("SUSSESSFULL INSERT", "YOUR CSV FILE HAS SUCCESFULLY INSERTED")
+            else:
+                msg.showerror("INSERT A CSV", "YOU HAVE TO INSERT A CSV FILE")
     def exitmenu(self):
         """ exit menu function """
         if msg.askokcancel("Quit?", "Really quit?"):

ead73ddd372039520fd646005c25c83e7c73a4a7
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-12-06 10:55:10 +0200

    Update csv_file_cleaner_v2.py

--- csv_file_cleaner_v2.py
+++ csv_file_cleaner_v2.py
@@ -27,6 +27,9 @@ class CsvFileCleaner():
                                    accelerator='Ctrl+F4', command=self.closefile)
         self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
         self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.remove_menu = Menu(self.menu, tearoff=0)
+        self.remove_menu.add_command(label="Remove column")
+        self.menu.add_cascade(label="Remove", menu=self.remove_menu)
         self.show_menu = Menu(self.menu, tearoff=0)
         self.show_menu.add_command(label="Show names of columns",
                                    accelerator='Alt+T',
